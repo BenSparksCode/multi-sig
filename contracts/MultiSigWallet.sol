ce41d8f376745f8b389725909d29a44c5c62d994
Ben Sparks <bensparks0@gmail.com>
2021-05-22 12:54:08 +0200

    Added events

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -19,6 +19,18 @@ contract MultiSigWallet {
 
     Transaction[] transactions;
 
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint256 indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
+
     modifier onlyOwner() {
         require(isOwner[msg.sender], "not owner");
         _;
